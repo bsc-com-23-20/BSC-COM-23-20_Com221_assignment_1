8d00a3a19d5433916d99d8c0b91ff98a55f9db90
bsc-com-23-20 <bsc-com-23-20@unima.ac.mw>
2023-04-18 08:58:38 +0100

    completed the person class, implemented getters and setters

--- Person.java
+++ Person.java
@@ -1,10 +1,54 @@
 public class Person{
    
    /*attributes of the Person class*/
-    String personName = "";
-String pAddress = "";
-int phoneNumber;
-String emailAdress = "";
+    private String personName = "";
+ private String pAddress = "";
+private int phoneNumber;
+private String emailAddress = "";
+
+//constructor
+public Person(String personName, String pAddress, int phoneNumber, String emailAddress){
+    this.personName = personName;
+    this.pAddress = pAddress;
+    this.phoneNumber = phoneNumber;
+    this.emailAddress = emailAddress;
 
 
+}
+
+//getters
+public String getPersonName(){
+    return personName;
+}
+public String getPAddress(){
+    return pAddress;
+
+}
+public int getPhoneNumber(){
+    return phoneNumber;
+}
+public String getEmailAddress(){
+    return emailAddress;
+}
+
+//setters
+public void setPersonName(String personName){
+    this.personName = personName;
+}
+public void setPAddress(String pAddress){
+    this.pAddress = pAddress;
+
+}
+public void setPhoneNumber(int phoneNumber){
+    this.phoneNumber = phoneNumber;
+}
+public void setEmailAddress(String emailAddress){
+    this.emailAddress = emailAddress;
+
+}
+//overriding the toString method
+
+    public String toString() {
+        return "Person: " + personName;
+    }
 }
---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test{
+    public static void main(String[] args){
+        Person person1 = new Person("Mercy Nago", "namiwawa 43 St", 899023459 , "mercynagoli@gmail.com");
+        System.out.println(person1);
+    }
+}
